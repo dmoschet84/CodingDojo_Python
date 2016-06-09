1c1978f834c1c64b7a9c79f5c5c3f11555289b2b
David Moschetti <dmoschet@gmail.com>
2016-06-09 14:28:45 -0500

    drawing stars function

---
+++ drawStars.py
@@ -0,0 +1,11 @@
+
+def draw_stars(mylist):
+    for item in mylist:
+        if (type(item) == int):
+            print("*" * item)
+        else:
+            print(item[0].lower()*len(item))
+
+x = [4, "David", 2, "Tess", 2, "Jessica", 10]
+
+draw_stars(x)
