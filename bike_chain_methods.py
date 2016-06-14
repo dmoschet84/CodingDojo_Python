0f4fe98dad5358804cbc473f880dd328aea04914
David Moschetti <dmoschet@gmail.com>
2016-06-14 15:45:25 -0500

    Update of bike and addition of bike w/chained methods

--- bike_chain_methods.py
+++ bike_chain_methods.py
@@ -9,32 +9,24 @@ class bike(object):
     print("Price - " + str(self.price) + " USD")
     print("Max Speed - " + str(self.max_speed) + " MPH")
     print("Miles - " + str(self.miles))
+    return self
   def ride(self):
     print("Riding...")
     self.miles += 10
+    return self
   def reverse(self):
     print("Reversing...")
     if (self.miles < 5):
       self.miles = 0
     else:
       self.miles -= 5
+    return self
 
 Huffy = bike(100,15)
-Huffy.ride()
-Huffy.ride()
-Huffy.ride()
-Huffy.reverse()
-Huffy.displayInfo()
+Huffy.ride().ride().ride().reverse().displayInfo()
 
 Schwinn = bike(300,20)
-Schwinn.ride()
-Schwinn.ride()
-Schwinn.reverse()
-Schwinn.reverse()
-Schwinn.displayInfo()
+Schwinn.ride().ride().reverse().reverse().displayInfo()
 
 Dyno = bike(200,15)
-Dyno.reverse()
-Dyno.reverse()
-Dyno.reverse()
-Dyno.displayInfo()
+Dyno.reverse().reverse().reverse().displayInfo()
